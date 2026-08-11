2251c42f41e63bb9aa659c40457ebdc9ec1350ed
drishtishakya23 <drishtishakya19@gmail.com>
2026-08-11 09:47:04 +0545

    category

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import type { Service } from "./types/service";
+import type { Category } from "./types/category";
+import FilterPill from "./components/filterPill";
 
 function App() {
   const [services, setServices] = useState<Service[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [categoryLoading, setCategoryLoading] = useState(true);
+  const [categoryError, setCategoryError] = useState("");
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   const fetchServices = async () => {
     try {
@@ -27,14 +35,53 @@ function App() {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      setCategoryLoading(true);
+      setCategoryError("");
+
+      const response = await fetch("http://localhost:3000/categories");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch categories");
+      }
+
+      const data: Category[] = await response.json();
+
+      setCategories(data);
+    } catch (error) {
+      setCategoryError("Failed to load categories");
+    } finally {
+      setCategoryLoading(false);
+    }
+  };
+
   useEffect(() => {
     fetchServices();
+    fetchCategories();
   }, []);
 
   return (
     <div>
       <h1>ServiceApp</h1>
       <p>Local service directory</p>
+
+      <div>
+        <FilterPill
+          label="All"
+          selected={selectedCategory === "All"}
+          onClick={() => setSelectedCategory("All")}
+        />
+
+        {categories.map((category) => (
+          <FilterPill
+            key={category.id}
+            label={category.label}
+            selected={selectedCategory === category.value}
+            onClick={() => setSelectedCategory(category.value)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
